41588fca8a04cf2dd99a8255cc14b507ab5ab6f1
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-12-28 13:59:41 +0530

    Fix the svg sprite

--- lib/plyr/rails.rb
+++ lib/plyr/rails.rb
@@ -2,6 +2,14 @@ require "plyr/rails/version"
 
 module Plyr
   module Rails
-    class Engine < ::Rails::Engine; end
+    class Engine < ::Rails::Engine
+        initializer :append_dependent_assets_path, :group => :all do |app|
+          app.config.assets.paths += %w( images )
+
+          app.config.assets.precompile += %w( plyr.css )
+          app.config.assets.precompile += %w( plyr.js )
+          app.config.assets.precompile += %w( plyr.svg )          
+        end  
+      end
   end
 end
